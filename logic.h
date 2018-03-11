71d2736a732933c263c2350041802d6131c9ec08
JohanGHole <36231049+JohanGHole@users.noreply.github.com>
2018-03-11 16:02:22 +0100

    kommentert (11.03.2018)

--- logic.h
+++ logic.h
@@ -1,6 +1,5 @@
 
 
-//extern int order_matrix[3][4];
 /*
 ____SET AND GET FUNCTIONS____
 */
@@ -12,6 +11,7 @@ void set_current_dir(int dir);
 int get_current_dir();
 void set_last_dir(int dir);
 int get_last_dir();
+
 /*
 ___FUNCTIONS FOR SETTING NEXT FLOOR BASED ON CURRENT STATE___
 */
@@ -20,23 +20,25 @@ int check_order();
 
 //Checks all orders above, including current floor. 
 //Prioritizes floor orders "up" and orders from the elevator panel.
-//set next floor based on nearest order and returns 1
+//set next floor based on nearest and most favorable order and returns 1
 //returns 0 if no order above
 int check_order_above(); 
 
 //checks all order below, including current floor.
 //prioritizes floor orders "down" and orders from the elevator panel.
-//set next floor based on the most valid order below and returns 1
+//set next floor based on the nearest and most favorable order below and returns 1
 //returns 0 if no order below
 int check_order_below();
 
 //delete orders for a given floor
 void delete_order(int floor);
 
-//calls check_order_below() or check_order_below() based on current direction, last direction and current floor
+//calls check_order_below() or check_order_above() based on current direction, last direction and current floor
 void set_priority();
 
+//returns sum of orders in the order matrix
 int sum_of_orders();
+
 /*
 ____HELPFUNCTIONS FOR SYSTEMTESTING____
 */
@@ -49,5 +51,6 @@ void print_matrix();
 /*
 ____TIMEDELAY FUNCTION____
 */
-void time_delay(int number_of_seconds); //Hold-up function for the elevator door lamp. 
-                                        //Stalls the program for number_of_seconds while checking if the stop button is pressed
+//Hold-up function for the elevator door lamp. 
+//Stalls the program for number_of_seconds while checking if the stop button is pressed
+void time_delay(int number_of_seconds); 
