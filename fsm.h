77e168843daf3124fd02ca269c40528322fc5bc3
JohanGHole <36231049+JohanGHole@users.noreply.github.com>
2018-03-11 15:34:10 +0100

    litt kommentering (11.03.2018)

--- fsm.h
+++ fsm.h
@@ -1,17 +1,23 @@
 #ifndef _INCLUDE_FSM_H_
 #define _INCLUDE_FSM_H_
-// Headerfil til tilstandsmaskinen
+// Headerfile for the statemachine
 
 
-
+//runs the elevator upwards until it reaches a floor. If it is already in a floor, does nothing
 void start();
 
+//moves the elevator up or down based on where next_floor is compared to current floor
+// sets motordirection "UP" or "DOWN"
 void move();
 
+//stops the elevator when it reaches next floor, lights the doorlamp for 3 seconds and sets idle state if sum_orders = 0
 void stop_floor();
 
+//initialized by stop button. Deletes all orders and sets door open lamp if the elevator is in a floor.
+// sets idle state
 void stop_button();
 
+// idle state. Check orders while sum_orders = 0
 void idle();
 
 #endif
